9d9db084b80f86a7a50dd2b16cb0b029de665bf0
anoop027 <74642411+anoop027@users.noreply.github.com>
2023-03-11 20:56:27 +0530

    Update chtag.sh

--- roll/chtag.sh
+++ roll/chtag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tag=$(sed -n '19p' /home/ubuntu/roll/dep.yml | awk -F '"' '{print $2}' | awk -F ':' '{print $2}')
+tag=$(sed -n '19p' dep.yml | awk -F '"' '{print $2}' | awk -F ':' '{print $2}')
 sed -i "19 s/$tag/$1/g" dep.yml
