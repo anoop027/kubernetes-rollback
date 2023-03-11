55d896e8ca5f044fbb1ab00b89aba8e408d80924
anoop027 <74642411+anoop027@users.noreply.github.com>
2023-03-11 21:03:39 +0530

    Update chtag.sh

--- roll/chtag.sh
+++ roll/chtag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tag=$(sed -n '19p' ./dep.yml | awk -F '"' '{print $2}' | awk -F ':' '{print $2}')
-sed -i "19 s/$tag/$1/g" ./dep.yml
+tag=$(sed -n '19p' /var/lib/jenkins/workspace/EKS-rollback/roll/dep.yml | awk -F '"' '{print $2}' | awk -F ':' '{print $2}')
+sed -i "19 s/$tag/$1/g" /var/lib/jenkins/workspace/EKS-rollback/roll/dep.yml
